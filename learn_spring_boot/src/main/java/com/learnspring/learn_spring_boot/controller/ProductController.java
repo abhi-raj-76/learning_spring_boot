aea5fa5e11d37ae674c1978adcb10cfa6b92cac6
abhi-raj-76 <raj12oct2001@gmail.com>
2026-08-05 12:33:45 +0530

    adding POST feature

--- learn_spring_boot/src/main/java/com/learnspring/learn_spring_boot/controller/ProductController.java
+++ learn_spring_boot/src/main/java/com/learnspring/learn_spring_boot/controller/ProductController.java
@@ -3,6 +3,9 @@ package com.learnspring.learn_spring_boot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +21,22 @@ public class ProductController
      public List<Product> getProducts()
      {
         return service.getProducts();
+     }
+     @RequestMapping("/product/{id}")
+     public Product getProductById(@PathVariable int id)
+     {
+        return service.getProductById(id);
+     }
+
+     //@RequestMapping("/products") 
+     @PostMapping("/products")
+
+     /* for the same url how can it identify which request is coming so 
+     we have two options 1. change the url for each diff request 2. use 
+     specialized annotations 
+     for get - GetMapping, for post - PostMapping .... */
+     public void addProduct(@RequestBody Product prod)
+     {
+        service.addProduct(prod);
      }
 }
--- learn_spring_boot/src/main/java/com/learnspring/learn_spring_boot/service/ProductService.java
+++ learn_spring_boot/src/main/java/com/learnspring/learn_spring_boot/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.learnspring.learn_spring_boot.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,13 +12,25 @@ import com.learnspring.learn_spring_boot.Product;
 we don't have to create the object via on your own */
 public class ProductService 
 {
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
         new Product(101,"iphone",5000),
         new Product(102,"camera",5000)
-    );
+    ));
 
     public List<Product> getProducts()
     {
         return products;
+    }
+
+    public Product getProductById(int id) 
+    {
+        return products.stream()
+        .filter(p -> p.getProId() == id)
+        .findFirst().get(); /*stream API */
+    }
+
+    public void addProduct(Product prod)
+    {
+        products.add(prod);
     }
 }
